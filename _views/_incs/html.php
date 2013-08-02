9956aed6ff5f846f24cecdb07102acad35680eff
Sam Acuna <samueleishion@gmail.com>
2013-08-02 15:00:42 -0500

    added html headers and footer to view includes

---
+++ _views/_incs/html.php
@@ -0,0 +1,18 @@
+<?
+
+function HTMLhead($page) {
+	?>
+<!DOCTYPE html>
+<html><head>
+ <title>Descartes : <? echo $page; ?></title>
+</head><body>
+	<?
+}
+
+function HTMLfoot() {
+	?>
+</body></html>
+	<?
+}
+
+?>
