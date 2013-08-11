6f07e05ff557fd623f863adcac7dc8e4d27851c4
Sam Acuna <samueleishion@gmail.com>
2013-08-11 14:05:45 -0500

    Added controller.

--- _controllers/controller.php
+++ _controllers/controller.php
@@ -1,2 +1,27 @@
 <?
+
+/*
+ * Descartes PHP Framework
+ * 	controller.php
+ * 
+ * @author: Samuel Acuna
+ * @date: 08/2013
+ * 
+ * Model builder and view selector.   
+ * 
+ */
+
+$view = (isset($_GET['view'])) ? cleanView($_GET['view']) :'home'; 
+switch($view) {
+	case 'admin':
+		echo 'Admin'; 
+		break; 
+	case 'project':
+		echo 'Project'; 
+		break; 
+	default:
+		echo 'Welcome!'; 
+		break; 
+}
+
 ?>
--- index.php
+++ index.php
@@ -13,9 +13,9 @@
  * 
  */
 
-include_once('_controllers/_libs/functions.php'); 
+include_once('_controllers/_libs/functions.php');
+include_once('_controllers/_libs/html.php');  
 require_once('_controllers/settings.php'); 
-
-
+require_once('_controllers/controller.php'); 
 
 ?>
