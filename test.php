18e08183d04fa6de10c659d47e12edab2b8836be
Sam Acuna <samueleishion@gmail.com>
2013-08-11 19:44:55 -0500

    Added Project model and tests for it.

--- test.php
+++ test.php
@@ -3,6 +3,9 @@
 require_once('_controllers/_libs/functions.php'); 
 require_once('_controllers/settings.php');
 require_once('_models/model.Image.php'); 
+require_once('_models/model.Project.php'); 
+echo "IMAGE".PHP_EOL; 
+echo "=====".PHP_EOL; 
 $img = new Image($dblink); 
 echo $img.PHP_EOL;
 
@@ -22,6 +25,34 @@ $img->save();
 echo $img.PHP_EOL; 
 
 $img->delete(); 
-echo $img.PHP_EOL;  
+echo $img.PHP_EOL.PHP_EOL;  
 
-?>
+echo "PROJECT".PHP_EOL; 
+echo "=======".PHP_EOL; 
+$pro = new Project($dblink); 
+echo $pro.PHP_EOL; 
+
+$pro->setName("metaphysics");
+$pro->setDescription("A description thing");  
+echo $pro.PHP_EOL; 
+
+$pro->instantiate(2); 
+echo $pro.PHP_EOL; 
+
+$pro->clear(); 
+$pro->setName("Ethics"); 
+$pro->setDescription("What is Good?"); 
+$pro->setTypes("2"); 
+$pro->addType("3"); 
+$pro->removeType("2"); 
+$pro->setCover(1); 
+echo $pro.PHP_EOL; 
+
+$pro->save(); 
+echo $pro.PHP_EOL; 
+
+$pro->delete(); 
+echo $pro.PHP_EOL; 
+
+
+?>  
