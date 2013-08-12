90d07403f5f5f288084b2b4cd643e2a7fb98833f
Sam Acuna <samueleishion@gmail.com>
2013-08-12 11:53:01 -0500

    removed some old files

--- _views/_incs/fs.php
+++
@@ -1,8 +0,0 @@
-<?
-
-function clean($str) { return htmlentities(stripslashes($str)); }
-function cleanView($str) { return strtolower(clean($str)); }
-function encode($str) { return hash('ripemd160',$str); } 
-function now() { return date('Y\/m\/d H\:i\:s'); }
-
-?>
