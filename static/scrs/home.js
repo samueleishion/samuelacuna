ef13d97a7214f7e9449029ec8c0295d8d8a84eeb
Sam Acuna <samueleishion@gmail.com>
2019-07-04 15:55:50 -0500

    feat(2019): #13 starting to update site for 2019

--- static/scrs/home.js
+++ static/scrs/home.js
@@ -1,3 +1,25 @@
-$(document).ready(function() { 
-	
-}); 
+$(document).ready(function() {
+  var bgimgs = [
+    '../imgs/bg/california1_bw_sm.jpg',
+    '../imgs/bg/california2_bw_sm.jpg',
+    '../imgs/bg/california3_bw_sm.jpg',
+    '../imgs/bg/california4_bw_sm.jpg',
+    '../imgs/bg/california5_bw_sm.jpg',
+    '../imgs/bg/california6_bw_sm.jpg',
+    '../imgs/bg/newmexico3_bw_sm.jpg',
+    '../imgs/bg/texas_bw_sm.jpg',
+    '../imgs/bg/utah_bw_sm.jpg'
+  ];
+
+  var getBgimg = function() {
+    var index = Math.round(Math.random() * (bgimgs.length - 1));
+    var img = bgimgs[index];
+    console.log("getBgimg", index, img, bgimgs.length);
+    return 'url("' + img + '")';
+  };
+
+  var img = getBgimg();
+
+  console.log(img);
+  $('body').attr('style', 'background-image:' + img);
+});
